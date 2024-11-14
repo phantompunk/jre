9476f5ca1956a41b1949d1d8b2dfca9807129d08
Rodrigo Moran <rigo.moran0@gmail.com>
2024-11-14 23:44:51 +0100

    refactor: database package and include tests

---
+++ sql/database.go
@@ -0,0 +1,111 @@
+package sql
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+	"log"
+	"time"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type Time struct {
+	T time.Time
+}
+
+type Quote struct {
+	ID         int    `json:"id"`
+	ShortID    string `json:"shortId"`
+	Quote      string `json:"quote"`
+	Speaker    string `json:"speaker"`
+	Source     string `json:"source"`
+	SourceLink string `json:"sourceLink"`
+	Created    Time   `json:"date"`
+}
+
+type QuoteStore interface {
+	GetQuoteByID(ctx context.Context, id string) (*Quote, error)
+	GetRandomQuote(ctx context.Context) (*Quote, error)
+}
+
+type Database struct {
+	DB                 *sql.DB
+	url                string
+	maxOpenConnections int
+	maxIdleConnections int
+	log                *log.Logger
+}
+
+type DatabaseOption func(*Database)
+
+func WithBaseUrl(url string) DatabaseOption {
+	return func(c *Database) {
+		c.url = url
+	}
+}
+
+func WithLogger(log *log.Logger) DatabaseOption {
+	return func(c *Database) {
+		c.log = log
+	}
+}
+
+func NewDatabase(opt ...DatabaseOption) *Database {
+	d := new(Database)
+	for _, o := range opt {
+		o(d)
+	}
+
+	if d.maxIdleConnections == 0 {
+		d.maxIdleConnections = 10
+	}
+
+	if d.maxOpenConnections == 0 {
+		d.maxOpenConnections = 10
+	}
+	return d
+}
+
+func (d *Database) Connect() error {
+
+	var err error
+	d.DB, err = sql.Open("sqlite3", d.url)
+	if err != nil {
+		return err
+	}
+
+	d.DB.SetMaxIdleConns(d.maxIdleConnections)
+	d.DB.SetMaxOpenConns(d.maxOpenConnections)
+	return nil
+}
+
+func (d *Database) GetQouteById(ctx context.Context, id string) (*Quote, error) {
+	var q Quote
+	query := "SELECT id, quote FROM quotes WHERE id = ?;"
+
+	var args []any
+	args = append(args, id)
+	err := d.DB.QueryRowContext(ctx, query, args...).Scan(&q.ShortID, &q.Quote)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, fmt.Errorf("Error occured while querying for quote id:%v, %v", id, err.Error())
+		}
+		return nil, fmt.Errorf("Unknown error occured querying database for id:%v, %v", id, err.Error())
+	}
+	return &q, nil
+}
+
+func (d *Database) GetRandomQoute(ctx context.Context) (*Quote, error) {
+	var q Quote
+	query := "SELECT id, quote FROM quotes ORDER BY RANDOM() LIMIT 1;"
+
+	err := d.DB.QueryRowContext(ctx, query).Scan(&q.ShortID, &q.Quote)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, fmt.Errorf("Error occured while querying for random quote: %v", err.Error())
+		}
+		return nil, fmt.Errorf("Unknown error occured querying database: %v", err.Error())
+	}
+	return &q, nil
+}
---
+++ sql/database_test.go
@@ -0,0 +1,50 @@
+package sql
+
+import (
+	"context"
+	"testing"
+
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/stretchr/testify/require"
+)
+
+// CreateDatabase for testing.
+func CreateDatabase(t *testing.T) *Database {
+	t.Helper()
+
+	t.Log("CreatingDB")
+	db := NewDatabase(WithBaseUrl("../database/db.db"))
+	// sqlite, err := sql.Open("sqlite3", "./database/db.db")
+	// if err != nil {
+	// 	t.Log("Not able to open database", err.Error())
+	// 	t.Fatal(err)
+	// }
+	// db.DB = sqlite
+	t.Log("CreatedDB", db)
+	if err := db.Connect(); err != nil {
+		t.Fatal(err)
+	}
+	return db
+}
+
+func TestGetRandomQuote(t *testing.T) {
+	t.Run("Get a Random Quote", func(t *testing.T) {
+		db := CreateDatabase(t)
+		require.NotNil(t, db)
+		quote, err := db.GetRandomQoute(context.Background())
+		require.NoError(t, err)
+		require.NotEmpty(t, quote.Quote)
+	})
+}
+
+func TestGetQuoteByID(t *testing.T) {
+	t.Run("Get Quote By ID", func(t *testing.T) {
+		db := CreateDatabase(t)
+		require.NotNil(t, db)
+		id := "ba28b5"
+		quote, err := db.GetQouteById(context.Background(), id)
+		// require.Equal(t, id, quote.ShortID)
+		require.NoError(t, err)
+		require.NotEmpty(t, quote.Quote)
+	})
+}
